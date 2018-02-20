007d12cc0dbefa519bc2a6fdc4a1453e9de703ff
Maxime Dendauw <maxime@responsestudios.com>
2018-02-20 19:28:16 +0100

    new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,8 +87,8 @@
                 </div>
             @endif
 
-            <div class="content">
-          
+
+
 
                   <div id="signature-pad" class="signature-pad">
                      <div class="signature-pad--body">
@@ -114,15 +114,6 @@
                    </div>
 
 
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
         </div>
 
         <script src="js/signature_pad.js"></script>
