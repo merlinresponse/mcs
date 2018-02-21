27c9c205d13b366b6744929121bf75cb44c68d6e
Maxime Dendauw <maxime@responsestudios.com>
2018-02-21 09:12:50 +0100

    new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -104,7 +104,11 @@
                            <button type="button" class="button" data-action="undo">Undo</button>
 
                          </div>
-                  
+                         <div>
+                           <button type="button" class="button save" data-action="save-png">Save as PNG</button>
+                           <button type="button" class="button save" data-action="save-jpg">Save as JPG</button>
+                           <button type="button" class="button save" data-action="save-svg">Save as SVG</button>
+                         </div>
                        </div>
                      </div>
                    </div>
