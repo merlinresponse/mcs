c0249e91c4d10521d05233198eff995688a28252
Maxime Dendauw <maxime@responsestudios.com>
2018-06-13 07:48:00 +0200

    new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,10 +87,6 @@
 
         <a-camera>
           <a-cursor id="cursor">
-            <a-animation begin="click" easing="ease-in" attribute="scale"
-                     fill="backwards" from="0.1 0.1 0.1" to="1 1 1" dur="150"></a-animation>
-            <a-animation begin="cursor-fusing" easing="ease-in" attribute="scale"
-                     from="1 1 1" to="0.1 0.1 0.1" dur="1500"></a-animation>
           </a-cursor>
         </a-camera>
       </a-scene>
